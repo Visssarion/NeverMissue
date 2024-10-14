ea050d19301f9bd686fd8d750dcb24300c66d7ab
Vissa <46628714+Visssarion@users.noreply.github.com>
2024-10-14 13:12:36 +0300

    Removed Swing

--- client/src/main/java/dev/vissa/nevermissue/client/App.java
+++ client/src/main/java/dev/vissa/nevermissue/client/App.java
@@ -1,17 +1,9 @@
 package dev.vissa.nevermissue.client;
 
-import javax.swing.JFrame;
-
 public class App 
 {
     public static void main( String[] args )
     {
-    	JFrame testWindow = new JFrame("NeverMissue");
-		testWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		testWindow.setSize(400, 300);
-		
-		
-		testWindow.setVisible(true);
 		
     }
 }
