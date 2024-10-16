fb2fb7635c873fcff45ba6130d271a1f97c0d551
vissa <vlad5limar@gmail.com>
2024-10-16 14:57:33 +0300

    Update JRE version

--- client/src/main/java/dev/vissa/nevermissue/client/App.java
+++ client/src/main/java/dev/vissa/nevermissue/client/App.java
@@ -6,8 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
 import java.io.IOException;
 
 import dev.vissa.nevermissue.client.scenes.css.CSSResources;
--- server/src/main/java/dev/vissa/nevermissue/server/Database.java
+++ server/src/main/java/dev/vissa/nevermissue/server/Database.java
@@ -1,12 +1,5 @@
 package dev.vissa.nevermissue.server;
 
-import java.sql.Connection;
-import java.sql.Statement;
-
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Database {
 	private String IP;
 	private Integer port;
