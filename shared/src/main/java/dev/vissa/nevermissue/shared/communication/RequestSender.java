adea41dbd8ad002a421f54b8ca773fe9119983d6
vissa <vlad5limar@gmail.com>
2024-11-17 22:08:10 +0300

    Deprecate Request Sender

--- shared/src/main/java/dev/vissa/nevermissue/shared/communication/RequestSender.java
+++ shared/src/main/java/dev/vissa/nevermissue/shared/communication/RequestSender.java
@@ -2,6 +2,11 @@ package dev.vissa.nevermissue.shared.communication;
 
 import dev.vissa.nevermissue.shared.connection.Connection;
 
+/**Class that was made but wasn't used. Use connection.send(request.toString()) instead
+*
+*
+*/
+@Deprecated(forRemoval = true) 
 public class RequestSender {
 	private Connection connection;
 	
